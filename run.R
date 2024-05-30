e5906e5e703752b8366c33cc574039923ba17193
Simon Rolph <simon.p.rolph@gmail.com>
2024-05-30 17:00:22 +0100

    bigger text

--- run.R
+++ run.R
@@ -1,5 +1,4 @@
-
-
+# this script is triggered by the GitHub workflow
 
 #create a file name
 file_name <- paste0("report_",Sys.Date(),".html")
@@ -9,3 +8,6 @@ print("Generating report")
 rmarkdown::render("report_markdown.Rmd",
                   output_file = file_name,
                   output_dir = "reports")
+
+#copy the file to index.html
+file.copy(paste0("reports/",file_name),"index.html",overwrite=T)
